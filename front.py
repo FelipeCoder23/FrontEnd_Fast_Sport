8c30a22c26f3ec3e0964037d65a22fab8d1562d1
felipe alvarez <fe.alvarezdiaz@gmail.com>
2024-03-13 18:16:27 -0300

    update front.py

--- front.py
+++ front.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import requests
+import os
+
+
+# Establece la variable de entorno antes de cualquier otra lógica que la utilice
+os.environ['DEEPSORT_CONFIG_PATH'] = '/home/felipe-alvarez/code/Fast_final/yolov8/ultralytics/yolo/v8/segment/deep_sort_pytorch/configs/deep_sort.yaml'
 
 st.header('Obtener tiempos de partidos')
 url = "http://127.0.0.1:8000/predict_video"
